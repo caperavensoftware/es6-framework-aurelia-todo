25446d9c976873ff5bf8b8362bbb32f092962fe0
Mental Arena <caperaven@icloud.com>
2016-05-18 08:47:16 +0200

    main test update

---
+++ tests/unit/mainTests.js
@@ -0,0 +1,50 @@
+/*jshint strict: false */
+
+import chai from 'chai';
+import sinon from 'sinon';
+import {configure} from './../../src/main.js';
+
+const expect = chai.expect;
+
+class AureliaMock {
+    constructor() {
+        this.use = {
+            standardConfiguration() {
+                return this;
+            },
+            
+            developmentLogging() {
+                return this;
+            }            
+        };
+    }    
+        
+    start() {
+        return new Promise((resolve) => {
+           resolve(); 
+        });
+    }
+    
+    setRoot() {
+        return this;
+    }
+}
+
+describe('Main Tests', function() {
+    it ('configure', function() {
+        let aureliaMock = new AureliaMock();
+        
+        sinon.spy(aureliaMock.use, 'use.standardConfiguration');
+        sinon.spy(aureliaMock.use, 'developmentLogging');
+        sinon.spy(aureliaMock, 'start');
+        sinon.spy(aureliaMock, 'setRoot');
+        
+        configure(aureliaMock);
+        
+        expect(aureliaMock.use).to.be.not.null;
+        expect(aureliaMock.standardConfiguration.calledOnce).to.be.true;
+        expect(aureliaMock.developmentLogging.calledOnce).to.be.true;
+        expect(aureliaMock.start.calledOnce).to.be.true;
+        expect(aureliaMock.setRoot.calledOnce).to.be.true;
+    });
+});
