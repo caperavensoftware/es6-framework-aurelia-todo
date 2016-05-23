1f039b4fe68da63386ccd92e40f2fb6c0d156deb
caperaven@icloud.com <caperaven@icloud.com>
2016-05-23 16:12:45 +0200

    layout and templates

--- src/controls/search/searchInput.js
+++ src/controls/search/searchInput.js
@@ -1,6 +1,6 @@
-import {customElement, bindable} from 'aurelia-framework';
+import {customElement, bindable, bindingMode} from 'aurelia-framework';
 
 @customElement('search-input')
 export class SearchInput {
-    @bindable text;
+    @bindable({defaultBindingMode: bindingMode.twoWay}) text;
 }
--- src/welcome/welcome.js
+++ src/welcome/welcome.js
@@ -1,3 +1,20 @@
+import {bindable} from 'aurelia-framework';
+import TodoItem from './../models/todoItem';
+
 export class Welcome {
+    @bindable searchText;
+    @bindable items;
     
+    constructor() {
+        this.searchText = "search value";
+        this.items = [];
+        
+        this.items.push(new TodoItem("todo", new Date()));
+        this.items.push(new TodoItem("todo 2", new Date()));
+        this.items.push(new TodoItem("todo 3", new Date()));
+    }
+    
+    addClick() {
+        alert('click');
+    }
 }
